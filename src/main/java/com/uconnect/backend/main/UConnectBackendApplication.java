6e09322b1a80f71ce6d2f1ab67a4d63c1340966a
zjk97 <zhangjake97@gmail.com>
2021-10-29 00:42:08 -0400

    added a no-op unit test and some minor refactoring

--- src/main/java/com/uconnect/backend/main/UConnectBackendApplication.java
+++ src/main/java/com/uconnect/backend/main/UConnectBackendApplication.java
@@ -1,11 +1,11 @@
-package com.uconnectbackend.main;
+package com.uconnect.backend.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.uconnectbackend")
+@ComponentScan("com.uconnect.backend")
 public class UConnectBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/com/uconnect/backend/main/UConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uconnect.backend.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UConnectBackendApplicationTests {
+
+    @Test
+    void itWorks() {
+        System.out.println("UConnect serverside APIs are now available");
+    }
+}

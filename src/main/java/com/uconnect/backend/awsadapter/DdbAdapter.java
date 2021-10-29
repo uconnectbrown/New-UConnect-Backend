53650ee19f99c108fa96d1d1782215a91489a712
zjk97 <zhangjake97@gmail.com>
2021-10-29 00:48:01 -0400

    added support for DynamoDB with AWS SDK

---
+++ src/main/java/com/uconnect/backend/awsadapter/DdbAdapter.java
@@ -0,0 +1,69 @@
+package com.uconnect.backend.awsadapter;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class DdbAdapter {
+    private final AmazonDynamoDB ddbClient;
+    private DynamoDBMapper mapper;
+
+    public DdbAdapter(AmazonDynamoDB ddbClient) {
+        this.ddbClient = ddbClient;
+    }
+
+    public boolean createTableIfNotExists(String tableName, Class<?> clazz, long rcu, long wcu) {
+        mapper = new DynamoDBMapper(ddbClient);
+        CreateTableRequest request = mapper.generateCreateTableRequest(clazz);
+        request.setTableName(tableName);
+        request.setProvisionedThroughput(new ProvisionedThroughput(rcu, wcu));
+
+        return TableUtils.createTableIfNotExists(ddbClient, request);
+    }
+
+    public <T> void save(String tableName, T item) {
+        setMapperTableName(tableName);
+        mapper.save(item);
+    }
+
+    public <T> void delete(String tableName, T item) {
+        setMapperTableName(tableName);
+        mapper.delete(item);
+    }
+
+    public <T> List<T> query(String tableName, T item, Class clazz) {
+        setMapperTableName(tableName);
+        DynamoDBQueryExpression<T> queryExpression = new DynamoDBQueryExpression<T>().withHashKeyValues(item);
+
+        return mapper.query(clazz, queryExpression);
+    }
+
+    public <T> List<T> queryGSI(String tableName, String indexName, T item, Class clazz) {
+        setMapperTableName(tableName);
+        DynamoDBQueryExpression<T> queryExpression = new DynamoDBQueryExpression<T>()
+                .withIndexName(indexName)
+                .withHashKeyValues(item)
+                .withConsistentRead(false);
+
+        return mapper.query(clazz, queryExpression);
+    }
+
+    private void setMapperTableName(String tableName) {
+        DynamoDBMapperConfig config = DynamoDBMapperConfig
+                .builder()
+                .withTableNameOverride(DynamoDBMapperConfig.TableNameOverride.withTableNameReplacement(tableName))
+                .build();
+
+        mapper = new DynamoDBMapper(ddbClient, config);
+    }
+}
---
+++ src/main/java/com/uconnect/backend/main/AwsConfiguration.java
@@ -0,0 +1,35 @@
+package com.uconnect.backend.main;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.uconnect.backend.awsadapter.DdbAdapter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.uconnect.backend")
+@Slf4j
+public class AwsConfiguration {
+    @Bean
+    public AWSCredentialsProvider getAWSCredentialsProvider() {
+        return DefaultAWSCredentialsProviderChain.getInstance();
+    }
+
+    @Bean
+    public AmazonDynamoDB getAmazonDynamoDB(AWSCredentialsProvider awsCredentialsProvider) {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withCredentials(awsCredentialsProvider)
+                .withRegion("us-east-1")
+                .build();
+    }
+
+    @Bean
+    public DdbAdapter getDdbAdapter(AmazonDynamoDB dynamoDB) {
+        return new DdbAdapter(dynamoDB);
+    }
+}

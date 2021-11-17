7cbd4aab39373b2e163054c4a3770795ad32dcf7
David <dichlorodiphen@gmail.com>
2021-11-16 21:04:52 -0500

    Create DAO, service layer, and controller for Util and implement getEmails endpoint

---
+++ src/main/java/com/uconnect/backend/util/dao/UtilDAO.java
@@ -0,0 +1,37 @@
+package com.uconnect.backend.util.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uconnect.backend.awsadapter.DdbAdapter;
+import com.uconnect.backend.user.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UtilDAO {
+    @Autowired
+    private DdbAdapter ddbAdapter;
+
+    @Autowired
+    private String userTableName;
+    
+    /**
+     * Get all of the emails currently in the database.
+     * 
+     * Calls to this function should be avoided, as it makes use of the DDB scan operation.
+     * 
+     * @return A list of all emails in the database
+     */
+    public List<String> getEmails() {
+        List<String> emails = new ArrayList<>();
+        List<User> users = ddbAdapter.scan(userTableName, User.class);
+        for (User u : users) {
+            emails.add(u.getUsername());
+        }
+
+        return emails;
+    }
+    
+}

b9d5c3d98fe7d2878398f90a18c3f62f679d6add
David <dichlorodiphen@gmail.com>
2021-11-24 16:12:39 -0500

    Replace occurrences of user1 with sender and user2 with receiver in connect/*

--- src/main/java/com/uconnect/backend/connect/dao/ConnectDAO.java
+++ src/main/java/com/uconnect/backend/connect/dao/ConnectDAO.java
@@ -15,99 +15,99 @@ public class ConnectDAO {
     private String userTableName;
 
     /**
-     * Creates a request to connect from user1 to user2.
+     * Creates a request to connect from sender to receiver.
      * 
      * Exit codes:
      *   0 -- normal termination
-     *   -1 -- user1 has already sent a request to user2
-     *   -2 -- user2 has already received a request from user1
-     *   -3 -- user1 does not have a sufficient number of requests
+     *   -1 -- sender has already sent a request to receiver
+     *   -2 -- receiver has already received a request from sender
+     *   -3 -- sender does not have a sufficient number of requests
      * 
      * The operation is only successful on normal termination. All other cases will
-     * result in no changes to user1 and user2 in the database.
+     * result in no changes to sender and receiver in the database.
      * 
-     * @param username1 The username of user1
-     * @param username2 The username of user2
+     * @param senderUsername The username of sender
+     * @param receiverUsername The username of receiver
      * @return An exit code
      */
-    public int request(String username1, String username2) {
-        User user1 = ddbAdapter.findByUsername(username1);
-        User user2 = ddbAdapter.findByUsername(username2);
+    public int request(String senderUsername, String receiverUsername) {
+        User sender = ddbAdapter.findByUsername(senderUsername);
+        User receiver = ddbAdapter.findByUsername(receiverUsername);
 
-        // Add user2 to user1's sent list
-        if (user1.getSent().contains(username2)) {
-            return -1; // user1 has already sent a request to user2
+        // Add receiver to sender's sent list
+        if (sender.getSent().contains(receiverUsername)) {
+            return -1; // sender has already sent a request to receiver
         } else {
-            user1.getSent().add(username2);
+            sender.getSent().add(receiverUsername);
         }
 
-        // Add user1 to user2's pending list
-        if (user2.getPending().contains(username1)) {
+        // Add sender to receiver's pending list
+        if (receiver.getPending().contains(senderUsername)) {
             return -2; // should not get here in theory
         } else {
-            user2.getPending().add(username1);
+            receiver.getPending().add(senderUsername);
         }
 
-        // Decrement user1's requests
-        int requests = user1.getRequests();
+        // Decrement sender's requests
+        int requests = sender.getRequests();
         if (requests < 1) {
-            return -3; // user1 does not have a sufficient number of requests
+            return -3; // sender does not have a sufficient number of requests
         } else {
-            user1.setRequests(--requests);
+            sender.setRequests(--requests);
         }
 
         // Save
-        ddbAdapter.save(userTableName, user1);
-        ddbAdapter.save(userTableName, user2);
+        ddbAdapter.save(userTableName, sender);
+        ddbAdapter.save(userTableName, receiver);
 
         return 0;
     }
 
     /**
-     * Undoes a request to connect from user1 to user2.
+     * Undoes a request to connect from sender to receiver.
      * 
      * Exit codes:
      *   0 -- normal termination
-     *   -1 -- user1 has not sent a request to user2
-     *   -2 -- user2 has not received a request from user1
-     *   -3 -- user1 has too many requests
+     *   -1 -- sender has not sent a request to receiver
+     *   -2 -- receiver has not received a request from sender
+     *   -3 -- sender has too many requests
      * 
      * The operation is only successful on normal termination. All other cases will
-     * result in no changes to user1 and user2 in the database.
+     * result in no changes to sender and receiver in the database.
      * 
-     * @param username1 The username of user1
-     * @param username2 The username of user2
+     * @param senderUsername The username of sender
+     * @param receiverUsername The username of receiver
      * @return An exit code
      */
-    public int undoRequest(String username1, String username2) {
-        User user1 = ddbAdapter.findByUsername(username1);
-        User user2 = ddbAdapter.findByUsername(username2);
+    public int undoRequest(String senderUsername, String receiverUsername) {
+        User sender = ddbAdapter.findByUsername(senderUsername);
+        User receiver = ddbAdapter.findByUsername(receiverUsername);
 
-        // Remove user2 from user1's sent list
-        if (!(user1.getSent().contains(username2))) {
-            return -1; // user1 has already sent a request to user2
+        // Remove receiver from sender's sent list
+        if (!(sender.getSent().contains(receiverUsername))) {
+            return -1; // sender has already sent a request to receiver
         } else {
-            user1.getSent().remove(username2);
+            sender.getSent().remove(receiverUsername);
         }
 
-        // Remove user1 from user2's pending list
-        if (!(user2.getPending().contains(username1))) {
+        // Remove sender from receiver's pending list
+        if (!(receiver.getPending().contains(senderUsername))) {
             return -2; // should not get here in theory
         } else {
-            user2.getPending().remove(username1);
+            receiver.getPending().remove(senderUsername);
         }
 
-        // Increment user1's requests
-        int requests = user1.getRequests();
+        // Increment sender's requests
+        int requests = sender.getRequests();
         if (requests > 9) {
-            return -3; // user1 has too many requests (should not happen)
+            return -3; // sender has too many requests (should not happen)
         } else {
-            user1.setRequests(++requests);
+            sender.setRequests(++requests);
         }
 
         // Save
-        ddbAdapter.save(userTableName, user1);
-        ddbAdapter.save(userTableName, user2);
+        ddbAdapter.save(userTableName, sender);
+        ddbAdapter.save(userTableName, receiver);
 
         return 0;
     }

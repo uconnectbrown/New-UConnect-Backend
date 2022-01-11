bc4472f47cbdec3ef2786255395d2440c3a34f5c
David <dichlorodiphen@gmail.com>
2022-01-10 20:14:17 -0500

    Declare exceptions for methods in ConnectDAO and UserDAO

--- src/main/java/com/uconnect/backend/user/dao/UserDAO.java
+++ src/main/java/com/uconnect/backend/user/dao/UserDAO.java
@@ -68,8 +68,9 @@ public class UserDAO {
      * @param rawPassword The raw password of the user to update
      * @param user The user to update
      * @return Returns 0 if successful and -1 otherwise (user does not exist)
+     * @throws UserNotFoundException If the user is not found
      */
-    public int updateUser(String username, String rawPassword, User user) {
+    public int updateUser(String username, String rawPassword, User user) throws UserNotFoundException {
         if (ddbAdapter.findByUsername(username) == null) {
             // user does not exist
             return -1;

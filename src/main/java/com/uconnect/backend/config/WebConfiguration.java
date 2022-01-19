1c4af3c47536d4283969011e1a3611cbd35e3cad
zjk97 <zhangjake97@gmail.com>
2022-01-18 22:31:49 -0500

    fixed cors error, fixed db name for prod, updated oauth redirect uri

---
+++ src/main/java/com/uconnect/backend/config/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.uconnect.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://api.uconnectbrown.com")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
